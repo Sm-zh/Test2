a257f7f846a586df74029046b485365efed54793
S.zh <Seefmamd@gmail.com>
2023-02-12 21:46:03 +0300

    Built

---
+++ Sorter.java
@@ -0,0 +1,9 @@
+class Sorter {
+    public static void main(String[] args) {
+        selectionSort("a");
+    }
+    public static void selectionSort(String Array) {
+        System.out.println("aaa");
+
+    }
+}
