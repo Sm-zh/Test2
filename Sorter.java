24afe68446004d668107a666841329bdc442be2f
S.zh <Seefmamd@gmail.com>
2023-02-12 22:32:48 +0300

    ssuubb

--- Sorter.java
+++ Sorter.java
@@ -1,6 +1,6 @@
 class Sorter {
     public static void main(String[] args) {
-        selectionSort("sub");
+        selectionSort("ssuubb");
     }
     public static void selectionSort(String Array) {
         System.out.println("aaa");
