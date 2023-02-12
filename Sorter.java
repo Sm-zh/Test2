eaa52953f99f802d7361a406a8e3086df078e24e
S.zh <Seefmamd@gmail.com>
2023-02-12 22:02:57 +0300

    Y

--- Sorter.java
+++ Sorter.java
@@ -1,6 +1,6 @@
 class Sorter {
     public static void main(String[] args) {
-        selectionSort("a");
+        selectionSort("Y");
     }
     public static void selectionSort(String Array) {
         System.out.println("aaa");
